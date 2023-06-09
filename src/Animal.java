cb4bd2ce18e553e411c5c3f59e184c357efb530d
Francisco Mendezy <fmy@outlook.cl>
2023-06-09 14:24:32 -0400

    Primer commit con Poliformismo

---
+++ src/Animal.java
@@ -0,0 +1,29 @@
+class Animal {
+
+    public void generarSonido() {
+
+        System.out.println("Grr...");
+
+    }
+
+}
+
+class Gato extends Animal {
+
+    public void generarSonido() {
+
+        System.out.println("Miau");
+
+    }
+
+}
+
+class Perro extends Animal {
+
+    public void generarSonido() {
+
+        System.out.println("Guau");
+
+    }
+
+}
---
+++ src/Programa.java
@@ -0,0 +1,21 @@
+public class Programa
+
+{
+
+    public static void main (String[] args) {
+
+        Animal pelusa = new Perro();
+
+        Animal benito = new Gato();
+
+        System.out.print("El gato hace: ");
+
+        benito.generarSonido();
+
+        System.out.print("El perro hace: ");
+
+        pelusa.generarSonido();
+
+    }
+
+}
